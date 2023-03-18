f555e48cca2d9915fe106753960a7e28adaada87
alexferreiraofc <alex.ferreiraofc@gmail.com>
2023-03-18 11:03:45 -0300

    Inicio projeto

---
+++ src/main/java/com/alog/logistica/ApiAlogLogisticaApplication.java
@@ -0,0 +1,13 @@
+package com.alog.logistica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAlogLogisticaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiAlogLogisticaApplication.class, args);
+    }
+
+}
