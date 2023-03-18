ee20b2ad8c3dfd1f7ffa50bf30cd90a1865401bd
alexferreiraofc <alex.ferreiraofc@gmail.com>
2023-03-18 18:34:13 -0300

    feat: create method endpoint

--- src/main/java/com/alog/logistica/api/controller/CustomerController.java
+++ src/main/java/com/alog/logistica/api/controller/CustomerController.java
@@ -4,11 +4,9 @@ package com.alog.logistica.api.controller;
 import com.alog.logistica.domain.model.Customer;
 import com.alog.logistica.domain.repository.CustomerRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,12 +22,19 @@ public class CustomerController {
     public List<Customer> listar() {
         return customerRepository.findAll();
     }
-
     @GetMapping("/{customer_id}")
     public ResponseEntity<Customer> find(@PathVariable Long customer_id) {
         return customerRepository.findById(customer_id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Customer create(@RequestBody Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+
+
 
 }
