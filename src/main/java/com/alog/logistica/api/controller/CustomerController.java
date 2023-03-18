2afdbabb4a8fcfc8ef895780e608063668da9c5a
alexferreiraofc <alex.ferreiraofc@gmail.com>
2023-03-18 18:05:58 -0300

    feat: mapping for findbyId and change to fuctional code for less boilerplate.

--- src/main/java/com/alog/logistica/api/controller/CustomerController.java
+++ src/main/java/com/alog/logistica/api/controller/CustomerController.java
@@ -4,7 +4,10 @@ package com.alog.logistica.api.controller;
 import com.alog.logistica.domain.model.Customer;
 import com.alog.logistica.domain.repository.CustomerRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,12 +15,21 @@ import java.util.List;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/customers")
 public class CustomerController {
 
     private CustomerRepository customerRepository;
 
-    @GetMapping("/customers")
+    @GetMapping
     public List<Customer> listar() {
         return customerRepository.findAll();
     }
+
+    @GetMapping("/{customer_id}")
+    public ResponseEntity<Customer> find(@PathVariable Long customer_id) {
+        return customerRepository.findById(customer_id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
 }
