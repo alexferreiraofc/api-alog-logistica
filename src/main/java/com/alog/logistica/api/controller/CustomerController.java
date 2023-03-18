4f6b3ecb2da2d64359adaea2d24cab710e80dad4
alexferreiraofc <alex.ferreiraofc@gmail.com>
2023-03-18 18:47:05 -0300

    feat: delete endpoint and comments to the code.

--- src/main/java/com/alog/logistica/api/controller/CustomerController.java
+++ src/main/java/com/alog/logistica/api/controller/CustomerController.java
@@ -20,10 +20,12 @@ public class CustomerController {
 
     @GetMapping
     public List<Customer> listar() {
+        // list all customers
         return customerRepository.findAll();
     }
     @GetMapping("/{customer_id}")
     public ResponseEntity<Customer> find(@PathVariable Long customer_id) {
+        // find by id, check if exists, return ok if success or notFound if fail.
         return customerRepository.findById(customer_id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
@@ -36,14 +38,26 @@ public class CustomerController {
 
     @PutMapping("/{customer_id}")
     public ResponseEntity<Customer> update(@PathVariable Long customer_id, @RequestBody Customer customer) {
+        // check if exists id to update, if false, return notfound.
         if(!customerRepository.existsById(customer_id)) {
             return ResponseEntity.notFound().build();
         }
-
+        // success updating customer by id and saving
         customer.setId(customer_id);
         customer = customerRepository.save(customer);
 
         return ResponseEntity.ok(customer);
+    }
+
+    @DeleteMapping("/{customer_id}")
+    public ResponseEntity<Void> delete(@PathVariable Long customer_id) {
+        // check if exists id, if not, return notfound.
+        if (!customerRepository.existsById(customer_id)) {
+            return ResponseEntity.notFound().build();
+        }
+        // success deleting with void content aka noContent
+        customerRepository.deleteById(customer_id);
+        return ResponseEntity.noContent().build();
     }
 
 }
