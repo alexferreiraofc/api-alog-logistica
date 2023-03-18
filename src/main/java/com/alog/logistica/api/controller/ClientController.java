7cf3a86c9c9dec9a732b9e59cc89e29fb87968b0
alexferreiraofc <alex.ferreiraofc@gmail.com>
2023-03-18 17:00:02 -0300

    feat: new ClientRepository and changed all logic to the class.

--- src/main/java/com/alog/logistica/api/controller/ClientController.java
+++ src/main/java/com/alog/logistica/api/controller/ClientController.java
@@ -2,31 +2,22 @@ package com.alog.logistica.api.controller;
 
 
 import com.alog.logistica.domain.model.Client;
+import com.alog.logistica.domain.repository.ClientRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 
+@AllArgsConstructor
 @RestController
 public class ClientController {
+
+    private ClientRepository clientRepository;
+
     @GetMapping("/clientes")
     public List<Client> listar() {
-
-        var cl1 = new Client();
-        cl1.setId(1L);
-        cl1.setName("Alex");
-        cl1.setEmail("alex@mail.com");
-        cl1.setPhone("48 9999-0000");
-
-
-        var cl2 = new Client();
-        cl2.setId(2L);
-        cl2.setName("Maria");
-        cl2.setEmail("maria@mail.com");
-        cl2.setPhone("48 7777-9999");
-
-        return Arrays.asList(cl1, cl2);
+        return clientRepository.findAll();
     }
 }
---
+++ src/main/java/com/alog/logistica/domain/repository/ClientRepository.java
@@ -0,0 +1,10 @@
+package com.alog.logistica.domain.repository;
+
+import com.alog.logistica.domain.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+
+}
